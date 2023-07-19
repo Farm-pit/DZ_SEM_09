aa411fd1bff24ad1b8c241b940161fb903530867
Alexandr <skripkin.offical@mail.ru>
2023-07-19 13:18:35 +0300

    Решенин ДЗ 9

---
+++ TASK/task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text, string error = "Данные были введены ошибочно")
+{
+    try
+    {
+        System.Console.Write(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        System.Console.WriteLine(error);
+        return 0;
+    }
+
+}
+
+int CountToValue(int value1, int value2)
+{
+    if (value1 > value2) return 0;
+    return CountToValue(value1 + 1, value2) + value1;
+}
+
+
+void Main()
+{
+    int value1 = ReadInt("Введите число: ");
+    int value2 = ReadInt("Введите число: ");
+    if (value1 > value2) System.Console.WriteLine($"M = {value2}; N = {value1} -> {CountToValue(value2, value1)}" );
+    else System.Console.WriteLine($"M = {value1}; N = {value2} -> {CountToValue(value1, value2)}");
+
+
+}
+Main();
---
+++ TASK/task_68/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+ulong ReadInt(string text, string error = "Error")
+{
+    try
+    {
+        System.Console.Write(text);
+        return Convert.ToUInt64(Console.ReadLine());
+    }
+    catch
+    {
+      System.Console.WriteLine(error);
+      return 0;
+    }
+}
+  
+
+  ulong A(ulong n, ulong m)
+  {
+    if(n == 0) return m + 1;
+    else if(n != 0 && m == 0) return A(n - 1,1);
+    else return A(n -1,A(n,m - 1));
+    
+  }
+
+void Main()
+{
+ulong number1 = ReadInt("Введите число 1: ");
+ulong number2 = ReadInt("Введите число 2: ");
+
+System.Console.WriteLine(A(number1,number2));
+}
+
+Main();
+
